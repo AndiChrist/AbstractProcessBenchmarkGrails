7d4230f6513b8efe2e3a5d4b10f5e6cd52d0c19c
Andreas Christ <andichrist@gmx.de>
2012-10-01 15:08:56 +0200

    new session corrected

--- grails-app/controllers/abstractprocessbenchmarkgrails/SessionController.groovy
+++ grails-app/controllers/abstractprocessbenchmarkgrails/SessionController.groovy
@@ -16,21 +16,6 @@ class SessionController {
             return false
         }
     }
-    
-    def create() { // TODO
-        //[sessionInstance: new Session(params)]
-        
-        //def origSessionInstance = Session.get(params.id)
-        def sessionInstance = new Session(params)
-
-        if (!sessionInstance.save(flush: true)) {
-            render(view: "create", model: [sessionInstance: sessionInstance])
-            return
-        }
-        
-        // now load all Task objects from the process and create a result for each
-        //def tasks = 
-    }
 
     def list(Integer max) {
         params.max = Math.min(max ?: 10, 100)

309f3c383f95c2d09544e68cf669a396612d71e3
Andreas Christ <andichrist@gmx.de>
2012-10-11 15:29:41 +0200

    task sequence: update repaired

--- grails-app/controllers/abstractprocessbenchmarkgrails/TaskController.groovy
+++ grails-app/controllers/abstractprocessbenchmarkgrails/TaskController.groovy
@@ -29,11 +29,10 @@ class TaskController {
     
     def save() {
         def taskInstance = new Task(params)
-        println("params: " + params)
          
         // check sequence number: if it exists twice, take this seq-no as defined, and all other sequences must add +1
         def previousTasks = taskInstance.process.tasks
-        println("previousTasks: " + previousTasks)
+
         // easy approach
         previousTasks.each() {
             if (it.sequence >= taskInstance.sequence) {
@@ -47,8 +46,50 @@ class TaskController {
             return
         }
 
-        flash.message = message(code: 'default.created.message', args: [message(code: 'user.label', default: 'Task'), taskInstance.id])
+        flash.message = message(code: 'default.created.message', args: [message(code: 'task.label', default: 'Task'), taskInstance.id])
         redirect(action: "show", controller: "process",  id: taskInstance.process.id)
     }
     
+    def update(Long id, Long version) {
+        def taskInstance = Task.get(id)
+        def oldSeqNo = taskInstance.sequence
+        
+        if (!taskInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'task.label', default: 'Task'), id])
+            redirect(action: "list")
+            return
+        }
+
+        if (version != null) {
+            if (taskInstance.version > version) {
+                taskInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'task.label', default: 'Task')] as Object[],
+                          "Another task has updated this Task while you were editing")
+                render(view: "edit", model: [taskInstance: taskInstance])
+                return
+            }
+        }
+
+        taskInstance.properties = params
+
+        // check sequence number: if it exists twice, take this seq-no as defined, and all prior to that is reduced by 1
+        def previousTasks = taskInstance.process.tasks
+        
+        // easy approach
+        previousTasks.each() {
+            // from oldSeqNo (excluded!) to taskInstance.sequence decrease by 1 (except the edited instance)
+            if (it.sequence > oldSeqNo && it.sequence <= taskInstance.sequence && it.id != taskInstance.id) {
+                it.sequence -= 1
+                it.save(flush: true)
+            }
+        }
+        
+        if (!taskInstance.save(flush: true)) {
+            render(view: "edit", model: [taskInstance: taskInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'task.label', default: 'Task'), taskInstance.id])
+        redirect(action: "show", id: taskInstance.id)
+    }
 }
